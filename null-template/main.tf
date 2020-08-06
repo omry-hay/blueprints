ba9248a853971848cb932e4f4dc119a40d41611a
Omry Hay <46656490+omryhay1@users.noreply.github.com>
2020-08-06 15:01:28 +0300

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -4,6 +4,6 @@ resource "null_resource" "example1" {
 resource "null_resource" "example2" {
   provisioner "local-exec" {
     when    = "destroy"
-    command = "exit 1"
+    command = "echo `date`; exit 1"
   }
 }
