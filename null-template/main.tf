67c60a713bb790db893f9f2c6cfdcf96fe500e35
omryhay1 <omry@env0.com>
2020-12-21 14:37:33 +0200

    testing TF cloud

--- null-template/main.tf
+++ null-template/main.tf
@@ -3,3 +3,13 @@ resource "null_resource" "null" {
 
 resource "null_resource" "null2" {
 }
+
+terraform {
+  backend "remote" {
+    organization = "env0-prod"
+
+    workspaces {
+      name = "something-2"
+    }
+  }
+}
