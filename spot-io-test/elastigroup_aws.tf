9412696e80f5b6d99e816245ca208fd01672ea31
omryhay1 <omry@env0.com>
2021-01-20 14:16:56 +0200

    changed spot.io settings

--- spot-io-test/elastigroup_aws.tf
+++ spot-io-test/elastigroup_aws.tf
@@ -6,7 +6,7 @@ resource "spotinst_elastigroup_aws" "default-elastigroup" {
 
   max_size          = 4
   min_size          = 2
-  desired_capacity  = 3
+  desired_capacity  = 1
   capacity_unit     = "instance"
 
   region      = "us-east-1"
