8ec5295d305580ff98aa3ac01b18a1502161b659
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2021-04-13 16:06:21 +0300

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,14 +1,2 @@
 resource "null_resource" "null" {
 }
-
-resource "null_resource" "null2" {
-}
-
-output "jFrog_sensitive" {
-  sensitive = true
-  value = "omryhay"
-}
-
-output "not_sensitive" {
-  value = "omry1"
-}
