1422b0e8e0c3864575c44dd9fe1a7e4a9a37f26c
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2021-10-14 12:45:29 +0300

    Update main.tf

--- some/null-template/main.tf
+++ some/null-template/main.tf
@@ -1,4 +1,7 @@
-module "null-tamplate" {
-  source  = "app.terraform.io/env0-prod/null-tamplate/env0"
-  version = "0.0.1"
+# module "null-tamplate" {
+#   source  = "app.terraform.io/env0-prod/null-tamplate/env0"
+#   version = "0.0.1"
+# }
+
+resrouce "null-resource" "some-null" {
 }
