1f0cd2b34db788a86d3ef94f0cb56e864646de2c
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2022-06-29 23:53:10 +0300

    Create main.tf

---
+++ null-template/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "remote" {
+    organization = "env0-prod"
+
+    workspaces {
+      name = "testing-tfc-on-env0"
+    }
+  }
+}
+
+resource "null_resource" "null" {
+}
+
+
+resource "null_resource" "null3" {
+}
