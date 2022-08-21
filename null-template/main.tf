2a9ae5b535b5092f1bb9aaf4c46820b748e2bb79
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2022-08-21 17:02:25 +0300

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -11,7 +11,7 @@ output "jFrog_sensitive" {
 
 module "null-tamplate" {
   source  = "app.terraform.io/env0-prod/null-tamplate/env0"
-  version = "0.0.2"
+  version = "0.0.3"
 }
 
 output "not_sensitive" {
