321310f994ffe578079339dc7eeab7451fb1cef0
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2022-08-22 08:55:01 +0300

    Update main.tf

--- null-template/main.tf
+++ null-template/main.tf
@@ -9,11 +9,6 @@ output "jFrog_sensitive" {
   value = "omryhay"
 }
 
-module "null-tamplate" {
-  source  = "app.terraform.io/env0-prod/null-tamplate/env0"
-  version = "0.0.2"
-}
-
 output "not_sensitive" {
   value = "omry1"
 }
