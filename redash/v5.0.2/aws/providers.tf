00cdde66a690ac7593f0581f24ce55f732c33e21
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2023-01-21 12:08:25 +0200

    Update providers.tf

--- redash/v5.0.2/aws/providers.tf
+++ redash/v5.0.2/aws/providers.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region = "${var.aws_default_region}"
+  assume_role_with_web_identity {
+    role_arn                = "arn:aws:iam::170412844252:role/env0_oidc_role"
+    session_name            = "env0_OIDC_session"
+    web_identity_token_file = "web-identity-token.txt"
+  }
 }
 
 # Using these data sources allows the configuration to be
