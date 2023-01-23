b2455989e09af9e3775d3fe868c9c3ee8b2337a1
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2023-01-23 13:07:24 +0200

    Create main.tf

---
+++ gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.49.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("env0-client-configuration.json")
+
+  project = "env0project"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
