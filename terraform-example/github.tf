f921ff8ac97a0d5941083af104012c452c72be37
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2023-05-21 17:42:58 +0300

    Update github.tf

--- terraform-example/github.tf
+++ terraform-example/github.tf
@@ -2,11 +2,6 @@ data "github_repository" "omry_repo" {
   full_name = "omry-hay/blueprints"
 }
 
-resource "github_branch" "vpc" {
-  repository = data.github_repository.omry_repo.full_name
-  branch     = module.vpc.name
-}
-
 output "repo_name" {
   value = data.github_repository.omry_repo.homepage_url
 }
