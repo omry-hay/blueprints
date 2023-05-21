5b26efbbc09e2f766b2aeff84610c79c6c5ee214
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2023-05-21 17:43:16 +0300

    Update vpc.tf

--- terraform-example/vpc.tf
+++ terraform-example/vpc.tf
@@ -4,12 +4,13 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
+  single_nat_gateway  = true 
 
   tags = {
     Terraform = "true"
@@ -20,12 +21,11 @@ module "vpc" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = toset(["one", "two", "three"])
+  for_each = toset(["one", "two"])
 
   name = "instance-${each.key}"
 
   instance_type          = "t2.micro"
-  key_name               = "user1"
   monitoring             = true
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.private_subnets[0]
