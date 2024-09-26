03ed86bc6d3c2dcb185dccd7ec97dbd155dec528
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2024-09-26 16:50:11 +0300

    Update main.tf with a lot of providers

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,3 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    env0 = {
+      source = "env0/env0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "alicloud" {}
+
+provider "kubernetes"{}
+
+provider "azurerm" {}
+
+provider "auth0" {}
+
+provider "env0" {}
+
 resource "null_resource" "null" {
 }
 
